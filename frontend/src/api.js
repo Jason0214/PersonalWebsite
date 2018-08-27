4ab08f56ce9ff151902f6ec02dcf4374875aa740
Jason0214 <lujc.me@gmail.com>
2018-08-27 16:50:51 +0800

    :bug: fix api base address bug

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 export default () => {
   return axios.create({
-    baseURL: 'http://localhost:3000',
+    baseURL: '',
     headers: {
       'Accept': 'application/js',
       'Content-Type': 'application/js'
