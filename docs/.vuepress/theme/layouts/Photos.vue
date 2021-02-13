57a25b495d316fde51cf5623b72217a311ff3a5d
Jason0214 <lujc.me@gmail.com>
2021-02-13 23:21:09 +0800

    Add CC-BY-NC-SA license for photos.

--- docs/.vuepress/theme/layouts/Photos.vue
+++ docs/.vuepress/theme/layouts/Photos.vue
@@ -9,6 +9,16 @@
     <div class="sidebar-mask" @click="toggleSidebar(false)" />
     <Sidebar :items="sidebarItems" @toggle-sidebar="toggleSidebar" />
     <Content class="photos-container" />
+    <a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/2.0/">
+      <img
+        alt="Creative Commons License"
+        style="border-width: 0"
+        src="https://i.creativecommons.org/l/by-nc-sa/2.0/88x31.png" />
+    </a>
+    <br />This work is licensed under a
+    <a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/2.0/">
+      Creative Commons Attribution-NonCommercial-ShareAlike 2.0 Generic License
+    </a>.
   </div>
 </template>
 
